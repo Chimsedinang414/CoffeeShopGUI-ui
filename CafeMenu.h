b65ea4df72959fcb7dfdf5f8d4a226cccee92dd1
Chimsedinang414 <144652073+Chimsedinang414@users.noreply.github.com>
2025-03-28 22:39:50 +0700

    đã cập nhật lại giao diện

--- CafeMenu.h
+++ CafeMenu.h
@@ -68,6 +68,9 @@ namespace CppCLRWinFormsProject {
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ drink_name;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ price;
+	private: System::Windows::Forms::ListView^ listView1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::HelpProvider^ helpProvider1;
 
 
 
@@ -120,13 +123,16 @@ namespace CppCLRWinFormsProject {
 			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
-			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
-			this->tàiKhoảnToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->thôngTinCáNhânToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->đăngXuấtToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->drink_name = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->price = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->tàiKhoảnToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->thôngTinCáNhânToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->đăngXuấtToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->helpProvider1 = (gcnew System::Windows::Forms::HelpProvider());
 			this->contextMenuStrip1->SuspendLayout();
 			this->panel3->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
@@ -136,6 +142,7 @@ namespace CppCLRWinFormsProject {
 			this->tabControl1->SuspendLayout();
 			this->tabPage1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->tabPage2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// contextMenuStrip1
@@ -157,9 +164,9 @@ namespace CppCLRWinFormsProject {
 			// panel2
 			// 
 			this->panel2->BackColor = System::Drawing::SystemColors::Window;
-			this->panel2->Location = System::Drawing::Point(612, 114);
+			this->panel2->Location = System::Drawing::Point(615, 114);
 			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(433, 326);
+			this->panel2->Size = System::Drawing::Size(433, 320);
 			this->panel2->TabIndex = 7;
 			// 
 			// panel3
@@ -167,9 +174,9 @@ namespace CppCLRWinFormsProject {
 			this->panel3->Controls->Add(this->numericUpDown2);
 			this->panel3->Controls->Add(this->button3);
 			this->panel3->Controls->Add(this->button2);
-			this->panel3->Location = System::Drawing::Point(612, 446);
+			this->panel3->Location = System::Drawing::Point(612, 433);
 			this->panel3->Name = L"panel3";
-			this->panel3->Size = System::Drawing::Size(433, 72);
+			this->panel3->Size = System::Drawing::Size(433, 85);
 			this->panel3->TabIndex = 0;
 			// 
 			// numericUpDown2
@@ -201,6 +208,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			// panel4
 			// 
+			this->panel4->BackColor = System::Drawing::Color::SteelBlue;
 			this->panel4->Controls->Add(this->textBox1);
 			this->panel4->Controls->Add(this->button4);
 			this->panel4->Controls->Add(this->numericUpDown1);
@@ -232,10 +240,10 @@ namespace CppCLRWinFormsProject {
 			// 
 			// numericUpDown1
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(318, 63);
+			this->numericUpDown1->Location = System::Drawing::Point(318, 61);
 			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 100, 0, 0, System::Int32::MinValue });
 			this->numericUpDown1->Name = L"numericUpDown1";
-			this->numericUpDown1->Size = System::Drawing::Size(103, 22);
+			this->numericUpDown1->Size = System::Drawing::Size(68, 22);
 			this->numericUpDown1->TabIndex = 3;
 			// 
 			// button1
@@ -282,7 +290,7 @@ namespace CppCLRWinFormsProject {
 			this->tabControl1->Location = System::Drawing::Point(3, 3);
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(590, 466);
+			this->tabControl1->Size = System::Drawing::Size(590, 450);
 			this->tabControl1->TabIndex = 0;
 			// 
 			// tabPage1
@@ -292,19 +300,59 @@ namespace CppCLRWinFormsProject {
 			this->tabPage1->Location = System::Drawing::Point(4, 28);
 			this->tabPage1->Name = L"tabPage1";
 			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage1->Size = System::Drawing::Size(582, 434);
+			this->tabPage1->Size = System::Drawing::Size(582, 418);
 			this->tabPage1->TabIndex = 0;
 			this->tabPage1->Text = L"Đồ Uống";
 			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) {
+				this->drink_name,
+					this->price
+			});
+			this->dataGridView1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->dataGridView1->Location = System::Drawing::Point(3, 3);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(576, 412);
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// drink_name
+			// 
+			this->drink_name->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::Fill;
+			this->drink_name->HeaderText = L"Tên Đồ Uống";
+			this->drink_name->MinimumWidth = 6;
+			this->drink_name->Name = L"drink_name";
+			// 
+			// price
+			// 
+			this->price->HeaderText = L"Giá ";
+			this->price->MinimumWidth = 6;
+			this->price->Name = L"price";
+			this->price->Width = 125;
+			// 
 			// tabPage2
 			// 
+			this->tabPage2->Controls->Add(this->listView1);
 			this->tabPage2->Location = System::Drawing::Point(4, 28);
 			this->tabPage2->Name = L"tabPage2";
 			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(582, 434);
+			this->tabPage2->Size = System::Drawing::Size(582, 418);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"Ăn Vặt";
 			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// listView1
+			// 
+			this->listView1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->listView1->HideSelection = false;
+			this->listView1->Location = System::Drawing::Point(3, 3);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(576, 412);
+			this->listView1->TabIndex = 0;
+			this->listView1->UseCompatibleStateImageBehavior = false;
 			// 
 			// tàiKhoảnToolStripMenuItem
 			// 
@@ -324,41 +372,27 @@ namespace CppCLRWinFormsProject {
 			this->đăngXuấtToolStripMenuItem1->Size = System::Drawing::Size(224, 26);
 			this->đăngXuấtToolStripMenuItem1->Text = L"Đăng Xuất";
 			// 
-			// dataGridView1
+			// label1
 			// 
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) {
-				this->drink_name,
-					this->price
-			});
-			this->dataGridView1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->dataGridView1->Location = System::Drawing::Point(3, 3);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->RowTemplate->Height = 24;
-			this->dataGridView1->Size = System::Drawing::Size(576, 428);
-			this->dataGridView1->TabIndex = 0;
-			// 
-			// drink_name
-			// 
-			this->drink_name->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::Fill;
-			this->drink_name->HeaderText = L"Tên Đồ Uống";
-			this->drink_name->MinimumWidth = 6;
-			this->drink_name->Name = L"drink_name";
-			// 
-			// price
-			// 
-			this->price->HeaderText = L"Giá ";
-			this->price->MinimumWidth = 6;
-			this->price->Name = L"price";
-			this->price->Width = 125;
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::MediumOrchid;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Bauhaus 93", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(309, 12);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(153, 26);
+			this->label1->TabIndex = 10;
+			this->label1->Text = L"Cà Phê  DUT";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// CafeMenu
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->BackColor = System::Drawing::Color::DodgerBlue;
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
 			this->ClientSize = System::Drawing::Size(1057, 530);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->flowLayoutPanel1);
 			this->Controls->Add(this->panel4);
 			this->Controls->Add(this->panel3);
@@ -377,7 +411,9 @@ namespace CppCLRWinFormsProject {
 			this->tabControl1->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->tabPage2->ResumeLayout(false);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
--- Form1.h
+++ Form1.h
@@ -83,7 +83,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			// panel1
 			// 
-			this->panel1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->panel1->BackColor = System::Drawing::Color::Green;
 			this->panel1->Controls->Add(this->button2);
 			this->panel1->Controls->Add(this->button1);
 			this->panel1->Controls->Add(this->panel3);
@@ -98,7 +98,7 @@ namespace CppCLRWinFormsProject {
 			this->button2->BackColor = System::Drawing::SystemColors::Info;
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(459, 146);
+			this->button2->Location = System::Drawing::Point(451, 135);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(78, 42);
 			this->button2->TabIndex = 4;
@@ -111,7 +111,7 @@ namespace CppCLRWinFormsProject {
 			this->button1->BackColor = System::Drawing::SystemColors::Info;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(328, 146);
+			this->button1->Location = System::Drawing::Point(292, 135);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(96, 42);
 			this->button1->TabIndex = 3;
@@ -121,9 +121,10 @@ namespace CppCLRWinFormsProject {
 			// 
 			// panel3
 			// 
+			this->panel3->BackColor = System::Drawing::SystemColors::Highlight;
 			this->panel3->Controls->Add(this->textBox2);
 			this->panel3->Controls->Add(this->label2);
-			this->panel3->Location = System::Drawing::Point(10, 77);
+			this->panel3->Location = System::Drawing::Point(10, 65);
 			this->panel3->Name = L"panel3";
 			this->panel3->Size = System::Drawing::Size(546, 52);
 			this->panel3->TabIndex = 1;
@@ -142,7 +143,7 @@ namespace CppCLRWinFormsProject {
 			this->label2->BackColor = System::Drawing::SystemColors::MenuHighlight;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Arial", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(13, 15);
+			this->label2->Location = System::Drawing::Point(22, 17);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(89, 19);
 			this->label2->TabIndex = 0;
@@ -150,6 +151,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			// panel2
 			// 
+			this->panel2->BackColor = System::Drawing::SystemColors::MenuHighlight;
 			this->panel2->Controls->Add(this->textBox1);
 			this->panel2->Controls->Add(this->label1);
 			this->panel2->Location = System::Drawing::Point(10, 9);
